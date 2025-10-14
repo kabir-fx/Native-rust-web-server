3f9f9955c65b2a5861bdecaebf4dcd345ce3701a
Kabir <vatshivam49888@gmail.com>
2025-10-14 21:08:23 +0530

    Add check to ensure the client request is valid

--- src/main.rs
+++ src/main.rs
@@ -21,47 +21,48 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
 
     /*
-        Collecting the lines of request that the client sends to the server in a vector.
-
         HTTP is a text-based protocol, and a request takes this format:
 
-            Method Request-URI HTTP-Version CRLF
-            headers CRLF
-            message-body
+        Method Request-URI HTTP-Version CRLF
+        headers CRLF
+        message-body
 
-        Thus since the request and headers are separated by /r/n (new line) we will be breaking and collecting these into a vector until we reach we a new line.
+        Here, we are reading the first line of the reaquest - send into the stream to determine whether the request is GET and is for root (/).
     */
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let http_req: Vec<String> = buf_reader
-        .lines()
-        // Since lines() returns an interator of Result<String, Error> - we are mapping  each corresponding string to get its String value.
-        .map(|result| result.unwrap())
-        // The client signals the end of an HTTP request by sending two newline characters in a row, so to get one request from the stream, we take lines until we get a line that is the empty string.
-        .take_while(|line| !line.is_empty())
-        // Collect into a vector
-        .collect();
+    println!("Request: {request_line}");
 
-    println!("Request: {http_req:#>?}");
-
-    /*
+    if request_line == "GET / HTTP/1.1" {
+        /*
         Response Format:
 
-            HTTP-Version Status-Code Reason-Phrase CRLF
-            headers CRLF
-            message-body
+        HTTP-Version Status-Code Reason-Phrase CRLF
+        headers CRLF
+        message-body
 
         So, we are currently writing a success response with a 200 status code - no header or body.
-    */
-    let status_line: &str = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+        */
+        let status_line: &str = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
 
-    // To ensure a valid HTTP response, we add the Content-Length header which is set to the size of our response body, in this case the size of hello.html
-    let length = contents.len();
+        // To ensure a valid HTTP response, we add the Content-Length header which is set to the size of our response body, in this case the size of hello.html
+        let length = contents.len();
 
-    // Building the success response
-    let success_response_data =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        // Building the success response
+        let success_response_data =
+            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    // Writing the response as bytes back to stream to return to the client
-    stream.write_all(success_response_data.as_bytes()).unwrap();
+        // Writing the response as bytes back to stream to return to the client
+        stream.write_all(success_response_data.as_bytes()).unwrap();
+    } else {
+        // If the request for wither not GET or for ROOT we send a different page indicating the error.
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
