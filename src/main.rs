da8c161df245623724299d757885f6babd522c72
Kabir <vatshivam49888@gmail.com>
2025-10-14 21:22:00 +0530

    Remove code deduplication

--- src/main.rs
+++ src/main.rs
@@ -30,39 +30,32 @@ fn handle_connection(mut stream: TcpStream) {
         Here, we are reading the first line of the reaquest - send into the stream to determine whether the request is GET and is for root (/).
     */
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-
     println!("Request: {request_line}");
 
-    if request_line == "GET / HTTP/1.1" {
-        /*
+    /*
         Response Format:
 
-        HTTP-Version Status-Code Reason-Phrase CRLF
-        headers CRLF
-        message-body
+            HTTP-Version Status-Code Reason-Phrase CRLF
+            headers CRLF
+            message-body
 
-        So, we are currently writing a success response with a 200 status code - no header or body.
-        */
-        let status_line: &str = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-
-        // To ensure a valid HTTP response, we add the Content-Length header which is set to the size of our response body, in this case the size of hello.html
-        let length = contents.len();
-
-        // Building the success response
-        let success_response_data =
-            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        // Writing the response as bytes back to stream to return to the client
-        stream.write_all(success_response_data.as_bytes()).unwrap();
+        If the request matches our desired input we send then send the corresponding HTML and status code
+    */
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        // If the request for wither not GET or for ROOT we send a different page indicating the error.
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    // Reading the contents of the corresponding HTML file
+    let contents = fs::read_to_string(filename).unwrap();
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    // To ensure a valid HTTP response, we add the Content-Length header which is set to the size of our response body, equal to the size of corresponding HTML file.
+    let length = contents.len();
+
+    // Building the response to send to the client
+    let response_data = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    // Writing the response as bytes back to stream to return to the client
+    stream.write_all(response_data.as_bytes()).unwrap();
 }
