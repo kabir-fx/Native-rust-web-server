ee83d8920572495cc66aff7a30000758ece5e2b7
Kabir <vatshivam49888@gmail.com>
2025-10-16 00:13:14 +0530

    Add rust doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,33 @@ use std::{
     thread,
 };
 
+/// Data Structure to identify and store the threads for our server
 pub struct Worker {
+    /// Unique identifier for each thread
     id: usize,
+    /// Wrapping each thread inside an option since threads are later terminated when shutting down
     thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        // Create a new thread and assign it to Worker stuct
         let thread = thread::spawn(move || {
             loop {
+                // For the cuurent thread - acquires a mutex, blocking the current thread until the job is complete.
                 let message = receiver.lock().unwrap().recv();
 
+                // Match whther the result against its variants to ensure that the thread is valid.
                 match message {
+                    // If the thread is valid
                     Ok(job) => {
                         println!("Worker {id} got a job; executing.");
 
+                        // Perform the job/action
                         job();
                     }
+
+                    // If the session is terminated - the thread is then considered an error
                     Err(_) => {
                         println!("Worker {id} disconnected; shutting down.");
                         break;
@@ -36,7 +46,10 @@ impl Worker {
 }
 
 pub struct ThreadPool {
+    /// Vector of all the worker threads available for execution
     worker_instances: Vec<Worker>,
+
+    ///
     sender: Option<mpsc::Sender<Job>>,
 }
 
@@ -47,15 +60,20 @@ impl ThreadPool {
         // The `new` function will panic if the size is zero.
         assert!(size > 0);
 
+        // Create a MPSC channel for threads to communicate amongst each other
         let (sender, receiver) = mpsc::channel();
+        // Individual threads
         let receiver = Arc::new(Mutex::new(receiver));
 
+        // vector of defined size to store threads
         let mut workers = Vec::with_capacity(size);
 
         for i in 0..size {
+            // Push the threads into Worker struct
             workers.push(Worker::new(i, Arc::clone(&receiver)));
         }
 
+        // Initialize the thread-pool
         ThreadPool {
             worker_instances: workers,
             sender: Some(sender),
@@ -92,13 +110,16 @@ impl ThreadPool {
     }
 }
 
+// Graceful shutdown handling for threads
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        // Consumes the channel - takes the value out of the option, leaving a None in its place
         drop(self.sender.take());
 
         for worker in &mut self.worker_instances {
             println!("Shutting down worker {}", worker.id);
 
+            // Consumes each thread inside the channel
             if let Some(thread) = worker.thread.take() {
                 thread.join().unwrap();
             }
